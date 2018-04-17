08d41bc27f659ae2fd3df7faff5d555cef7e38db
Jacob Song <jacobsong@Jacobs-MacBook-Pro.local>
2018-04-16 23:59:53 -0400

    updated js

--- main.js
+++ main.js
@@ -1,30 +1,62 @@
 // listen for form submit
-// document.getElementById("jobform").addEventListener("submit", saveApp);
+document.getElementById("submit-button").addEventListener("click", saveApp);
 
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth()+1; //January is 0!
-var yyyy = today.getFullYear();
- if(dd<10){
-        dd='0'+dd
-    } 
-    if(mm<10){
-        mm='0'+mm
-    } 
+document.body.onload = function() {
+    setMaxDate();
+    fetchApps();
+}
 
-today = yyyy+'-'+mm+'-'+dd;
-document.getElementById("appdate").setAttribute("max", today);
+function setMaxDate() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd
+    } 
+    if (mm < 10) {
+        mm = '0' + mm
+    } 
+    today = yyyy+'-'+mm+'-'+dd;
+    document.getElementById("appdate").setAttribute("max", today);
+}
+
+function validateForm(comp, req, title, email, date) {
+    if (comp === "" || req === "" || title === "" || date === "") {
+        alert("You must fill out Company Name, Req ID, Job Title, and Date");
+        return false;
+    }
+    else if (date != "") {
+        
+    }
+    else if (email != "") {
+
+    }
+    else {
+        return true;
+    }
+}
 
 function saveApp() {
+    var company = document.getElementById('company').value;
     var reqid = document.getElementById('reqid').value;
     var jobtitle = document.getElementById('jobtitle').value;
+    var email = document.getElementById('email').value;
     var appdate = document.getElementById('appdate').value;
+    var status = document.getElementById('status').value;
+
+    if (!validateForm(company, reqid, jobtitle, email, appdate)) {
+        return false;
+    }
 
     // put form data in object
     var jobApp = {
+        company: company,
         req: reqid,
         title: jobtitle,
-        date: appdate
+        email: email,
+        date: appdate,
+        status: status
     }
 
     if (localStorage.getItem('apps') === null) {
